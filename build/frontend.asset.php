eaa0ed5d2174cf1d857d7534030a02a9f49708fa
Shalom Taiwo <shalomtaiwo@my.uopeople.edu>
2022-12-06 19:32:05 +0200

    chore: build file

--- build/frontend.asset.php
+++ build/frontend.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => 'c6765436868610301d02');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => '00600547c69afffe0327');
--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-components', 'wp-element'), 'version' => '77248232dc1517f370c5');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-components', 'wp-element'), 'version' => '58a38a335d0f99fe8821');
